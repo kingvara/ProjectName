83dfda8c220864ba21b2746fa5d8109d16e1009f
Varaprasath <varaprasath.s@cspl.com>
2026-07-10 12:13:10 +0530

    ProjectName

---
+++ src/main/java/org/yourcompany/yourproject/ProjectName.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author varaprasath.s
+ */
+public class ProjectName {
+
+  public static void main(String[] args) {
+
+        /*int countdown = 3;
+        while (countdown>0){
+            System.out.println(countdown);
+            countdown --;
+        }
+        System.out.println("Happy new year");/* */
+        /*int i=10;
+        while(i<8){
+            System.out.println("This is existed");
+        }/* */
+        for (int i=1; i<=3; i++) {
+            for(int j=1; j<=3; j++){
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
